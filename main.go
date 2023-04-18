8d4ed457a66eec786fd574920a97107a43cbf070
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-04-18 21:38:55 +0530

    code optimised

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"ccdata/ccdata"
 	"ccdata/mongo"
-	"fmt"
 	"os"
 
 	logger "github.com/rs/zerolog/log"
@@ -19,26 +18,40 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Push game data to MongoDB
+	err = pushDataToDB(gameData)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	logger.Info().Msg("Plugin executed successfully")
+}
+
+// pushDataToDB writes AssortedGamePGN data to a MongoDB collection in batches of 5000 games.
+//
+// Params:
+// gameData: A slice of AssortedGamePGN containing the game data to be written.
+//
+// Returns:
+// An error if the function fails to write the data to the collection.
+func pushDataToDB(gameData []ccdata.AssortedGamePGN) (err error) {
 	// Count the number of games found
 	gameCount := len(gameData)
-	logger.Info().Msg(fmt.Sprintf("Games found: %v", gameCount))
+	logger.Info().Msgf("Games found: %v", gameCount)
 
 	// Write game data to MongoDB "games" collection in batches of 5000 games
 	for i := 0; i < gameCount; i += 5000 {
 		// Determine the index of the last game in the batch
-		var index int
-		if i+5000 > gameCount {
+		index := i + 5000
+		if index > gameCount {
 			index = gameCount
-		} else {
-			index = i + 5000
 		}
 
 		// Write the batch of games to the "games" collection
-		err = mongo.WriteDataToCollection(os.Getenv("GAME_COLLECTION"), struct{ Games []ccdata.AssortedGamePGN }{Games: gameData[i:index]})
-		if err != nil {
+		if err = mongo.WriteDataToCollection(os.Getenv("GAME_COLLECTION"), struct{ Games []ccdata.AssortedGamePGN }{Games: gameData[i:index]}); err != nil {
 			// Log error and exit program
-			logger.Info().Err(err).Msg("error while writing data to collection")
-			os.Exit(1)
+			logger.Error().Err(err).Msgf("error while writing objects %v to %v", i, index)
 		}
 	}
+	return
 }
